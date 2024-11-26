4999d3b6d54e9c6d9e6435ac694b0c104188eca2
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-11-26 14:22:57 +0200

    update

--- index.php
+++ index.php
@@ -1,22 +1,33 @@
 <?php
 echo "Strada";
 
-$connection = new mysqli("localhost", "root", "root", "sipols");
+$connection = new mysqli("localhost", "root", "", "sipols");
 $result = $connection->query("SELECT * FROM users");
 $data = $result->fetch_assoc();
 var_dump($data);
 
-echo "<form>";
+echo "<pre>";
+var_dump($_SERVER);
+echo "</pre>";
 
 
 
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
+$sql = "INSERT INTO users
+        (username, pass)
+        VALUES
+        ('$username', '$password');
+";
+$connection->query($sql);   
+}
 
-
-
-
-
-
+echo "<form method='POST'>";
+    echo " <label>Lietotājvārds: <input name='username' placeholder='Lietotajvārds' /><label/><br>";
+    echo "<label>Parole: <input type='password' name='password' placeholder='Parole'/><label/><br>";
+    echo "<input type='submit' value='Reģistrēties'/>";
 echo "</form>";
 
 
